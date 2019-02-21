b49e2766716406ddfddcfe8c824b9d47c1b2b8be
Plabon Biswas <plaboniubat16@gmail.com>
2019-02-22 01:53:11 +0600

    Delete urls.py

--- urls.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-from django.urls import path, include
-from. import views
-
-urlpatterns = [
-    path('vote_home', views.vote_home, name='vote_home'),
-
-    path('vote/<int:pk>', views.vote, name='vote'),
-]
